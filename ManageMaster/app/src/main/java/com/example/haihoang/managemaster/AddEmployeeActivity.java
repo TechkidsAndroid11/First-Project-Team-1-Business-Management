fc7b1f7a135b4ba341e508d8150202a59964af27
Hai Hoang <haihmse04631>
2017-10-23 03:16:52 +0700

    upload UI

--- ManageMaster/app/src/main/java/com/example/haihoang/managemaster/AddEmployeeActivity.java
+++ ManageMaster/app/src/main/java/com/example/haihoang/managemaster/AddEmployeeActivity.java
@@ -1,17 +1,16 @@
 package com.example.haihoang.managemaster;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AddEmployeeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_employee);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.custom_action_bar_home);
-
     }
 }
--- ManageMaster/app/src/main/java/com/example/haihoang/managemaster/ListEmployeeActivity.java
+++ ManageMaster/app/src/main/java/com/example/haihoang/managemaster/ListEmployeeActivity.java
@@ -1,17 +1,17 @@
 package com.example.haihoang.managemaster;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ListEmployeeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_employee);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setCustomView(R.layout.custom_action_bar_home);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar_list_employee);
 
     }
 }
