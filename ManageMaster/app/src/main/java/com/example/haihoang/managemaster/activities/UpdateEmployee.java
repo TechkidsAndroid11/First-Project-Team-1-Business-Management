a3fab5f25b1ba1be58c9666ec4152724c9ba2d51
Hai Hoang <haihmse04631>
2017-10-28 12:53:58 +0700

    update code

---
+++ ManageMaster/app/src/main/java/com/example/haihoang/managemaster/activities/UpdateEmployee.java
@@ -0,0 +1,16 @@
+package com.example.haihoang.managemaster.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.haihoang.managemaster.R;
+
+public class UpdateEmployee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_employee);
+
+    }
+}
