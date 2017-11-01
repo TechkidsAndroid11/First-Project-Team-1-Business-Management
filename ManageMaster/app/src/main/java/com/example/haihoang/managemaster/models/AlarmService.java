a26bfbfaaf080e090c75312fea2c1e44b2a24475
ADMIN\Trần_Tân <tanmhtb@gmail.com>
2017-11-02 00:39:59 +0700

    update code

--- ManageMaster/app/src/main/java/com/example/haihoang/managemaster/models/AlarmService.java
+++ ManageMaster/app/src/main/java/com/example/haihoang/managemaster/models/AlarmService.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
@@ -25,6 +26,7 @@ import java.util.Date;
 
 public class AlarmService extends Service {
     private static final String TAG = "AlarmService";
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -33,7 +35,16 @@ public class AlarmService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
+        SharedPreferences sharedPreferences = getSharedPreferences("my_month",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        String myMonth = sharedPreferences.getString("month","");
+        String currtime = new SimpleDateFormat("MM").format(new Date());
+        if(myMonth.equals("")){
+            editor.putString("month",currtime);
+            myMonth = currtime;
+            editor.commit();
+        }
+        Log.d(TAG, "onStartCommand: "+myMonth);
         if(checkLastDayOfMonth()){
             Intent intent1 = new Intent(this, SummaryActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(this,(int)System.currentTimeMillis(),intent1,0);
@@ -48,7 +59,7 @@ public class AlarmService extends Service {
             noti.flags = Notification.FLAG_AUTO_CANCEL;
             manager.notify(0,noti);
         }
-        if (checkFistDayOfMonth()){
+        if (checkFistDayOfMonth(myMonth)){
             Log.d(TAG, "onStartCommand: reset");
             DatabaseHandle handle = DatabaseHandle.getInstance(this);
             ArrayList<EmployeeModel> model = handle.getAllEmployee();
@@ -59,8 +70,6 @@ public class AlarmService extends Service {
             handle.resetAllNote();
             handle.resetAllTotalSalary();
             handle.resetStatusAllAbsent();
-
-
         }
         return super.onStartCommand(intent, flags, startId);
     }
@@ -94,18 +103,29 @@ public class AlarmService extends Service {
         if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0)) return true;
         return false;
     }
-    public boolean checkFistDayOfMonth(){
+    public boolean checkFistDayOfMonth(String myMonth){
         Date date = new Date();
         String tmp = new SimpleDateFormat("dd/MM/yyyy").format(date);
-        Log.d(TAG, "checkFirstDayOfMonth: "+tmp+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds());
         String[] ntn=tmp.split("/");
+
+        SharedPreferences sharedPreferences = getSharedPreferences("my_month",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        Log.d(TAG, "checkFirstDayOfMonth: "+tmp+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()+": "+ntn[1]);
+
         int ngay = Integer.parseInt(ntn[0]);
         if(date.getHours()!=3||date.getMinutes()!=0){
             return false;
         }
-        if(ngay==1){
-            return true;
+        if (ngay == 1){
+            if(!myMonth.equals(ntn[1])){
+                editor.clear();
+                editor.putString("month",ntn[1]);
+                editor.commit();
+                return true;
+            }
         }
         return false;
     }
+
 }
